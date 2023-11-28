34fa260b03b73f58a3169e97edc9e4e589da3247
AlanPoAlsk <alsk91@gmail.com>
2023-11-28 08:33:08 -0500

    ninja assignment

---
+++ modern_js/ninja.js
@@ -0,0 +1,22 @@
+class Ninja {
+    constructor(name){
+        this.name = name;
+        this.health = 90;
+        this.speed = 3;
+        this.strength = 3;
+    }
+    sayName(){
+        console.log(this.name);
+    }
+    showStats(){
+        console.log(this.name,this.health,this.speed,this.strength)
+    }
+    drinkSake(){
+        this.health += 10;
+    }
+}
+const ninja1 = new Ninja("Jchan");
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
+ninja1.showStats();
